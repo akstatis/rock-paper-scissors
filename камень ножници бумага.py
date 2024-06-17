89224a76e7665ae730e7cc0543394e14ee96d94c
akstatis <solmatvei8@gmail.com>
2024-06-18 03:56:22 +1000

    Add files via upload

--- камень ножници бумага.py
+++ камень ножници бумага.py
@@ -1,14 +1,8 @@
 import random
 
-def question():
-    n = input("хотите сыграть еще раз?: ")
-    if n == "да":
-        code()
-    else:
-        print("До новых встречь!")
-        exit()
-
-def code():
+score_b = 0  # b = player
+score_a = 0  # a = computer
+while True:
     b = str(input("что ставите?(камень, ножницы, бумага): "))
     a = random.choice(["камень", "бумага", "ножницы"])
     print("программа поставила", a)
@@ -16,22 +10,27 @@ def code():
         print("ничья")
     elif a == "камень" and b == "ножницы":
         print("вы проиграли")
+        score_a += 1
     elif a == "камень" and b == "бумага":
         print("вы выйграли!")
+        score_b += 1
     elif a == "ножницы" and b == "камень":
         print("вы выйграли!")
+        score_b += 1
     elif a == "ножницы" and b == "бумага":
         print("вы проиграли")
+        score_a += 1
     elif a == "бумага" and b == "камень":
         print("вы проиграли")
+        score_a += 1
     elif a == "бумага" and b == "ножницы":
         print("вы выйграли!")
+        score_b += 1
     else:
         print("выберите что-либо из предоставленого списка")
-        code()
-    question()
-
-
-
-
-code()
+    print(f"ваш уровень: {score_b}")
+    print(f"уровень компьютера {score_a}")
+    n = input("хотите сыграть еще раз?: ")
+    if n == "нет":
+        print("До новых встречь!")
+        exit()
